d79ef31ef09ccae50bed188f0dbd4e53fe8ec3aa
J.D. Bean <jonathandbean@gmail.com>
2018-04-02 16:20:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinize.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :input
+  end
+
+  post '/' do
+    erb :results
+  end
+
 end
