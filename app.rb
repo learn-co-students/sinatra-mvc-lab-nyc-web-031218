6bdba2e0361e39c3d35abbe7ee3a7e372452585d
Manuel Grullon <astermanuelg@gmail.com>
2018-04-03 15:53:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @user_text = params[:user_phrase]
+
+    @piggy = PigLatinizer.new
+    @piggy_phrase = @piggy.piglatinize(@user_text)
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,18 @@
+class PigLatinizer
+  @@vowels = ['a','e','i','o','u']
+
+  def piglatinize (text)
+    text_array = text.split(" ")
+    results = text_array.map do |word|
+      if @@vowels.include?(word[0].downcase)
+        word + "way"
+      else
+        jumble = word.split(/([aeiou].*)/)
+        (jumble[1]+jumble[0]+"ay")
+      end
+    end
+    results.join(" ")
+  end
+
+
+end
